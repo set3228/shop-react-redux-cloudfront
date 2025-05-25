1956049629de02d88961134ba132f2a93134f2cc
Pavel Krautsou <Pavel_Krautsou1@epam.com>
2025-05-26 02:52:38 +0300

    integrate the app with import service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://uz5grq0ue2.execute-api.eu-north-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://5aduhr02b8.execute-api.eu-north-1.amazonaws.com/prod",
   bff: "https://uz5grq0ue2.execute-api.eu-north-1.amazonaws.com/prod",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
